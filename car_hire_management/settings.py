ff671ee9347388df7e4c2b7a8e10e745e6fa5af9
Shahen <shahengrig.stdev@gmail.com>
2021-04-01 19:03:53 +0400

    Added authentication

--- car_hire_management/settings.py
+++ car_hire_management/settings.py
@@ -105,9 +105,9 @@ REST_FRAMEWORK = {
      "DEFAULT_RENDERER_CLASSES": [
          'rest_framework.renderers.JSONRenderer',
      ],
-     "DEFAULT_AUTHENTICATION_CLASSES": [],
-     "DEFAULT_PERMISSION_CLASSES": [],
-     "UNAUTHENTICATED_USER": None,
+     "DEFAULT_AUTHENTICATION_CLASSES":
+         ["rest_framework_simplejwt.authentication.JWTAuthentication"],
+     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
      'PAGE_SIZE': 10,
      'DEFAULT_PAGINATION_CLASS': \
          'rest_framework.pagination.LimitOffsetPagination',
